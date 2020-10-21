8effed3b1234fab3fc9fcec3e2ac070247da7678
Brian Rhodes <brianr4548@gmail.com>
2020-10-21 22:57:30 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,5 +3,5 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-   array.count { |element| element.is_a?(String) && element.
+   array.count { |element| element.is_a?(String) && element.empty?
 end
